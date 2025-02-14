004afab1b7f9cff9bba879b6f1ee79c0cc8b57a2
Sayed Farzaan Rafi Bhat <kfn036@usask.ca>
2025-02-14 01:08:45 -0600

    Added a Job class with properties and methods

---
+++ app/src/main/java/com/example/myapplication/model/Job.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.model;
+
+public class Job {
+    private String title;
+    private String subTitle;
+    private String employer;
+    private String location;
+    private int color;
+
+    public Job(String title, String subTitle, String employer, String location, int color) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.employer = employer;
+        this.location = location;
+        this.color = color;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+    public void setSubTitle(String newSubTitle) {
+        subTitle = newSubTitle;
+    }
+
+    public String getEmployer() { return employer; }
+    public void setEmployer(String newEmployer) { employer = newEmployer; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String newLocation) { location = newLocation; }
+
+    public int getColor() { return color; }
+    public void setColor(int newColor) { color = newColor; }
+
+}
+
