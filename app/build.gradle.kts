a044e0d8e82b544e85b31db3919ffce91b6488d1
Sayed Farzaan Rafi Bhat <kfn036@usask.ca>
2025-02-15 10:47:13 -0600

    Setup the required metadata and the dependencies for this project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
