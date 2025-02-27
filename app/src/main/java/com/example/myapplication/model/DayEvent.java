8509fab9041cb7f87c355145d5042baee9526cb6
Parker <evz405@usask.ca>
2025-02-27 14:00:33 -0600

    added TimeEvent class, setting up constructors

--- app/src/main/java/com/example/myapplication/model/DayEvent.java
+++ app/src/main/java/com/example/myapplication/model/DayEvent.java
@@ -18,6 +18,7 @@ public class DayEvent {
 
     // for monthly repeated events, which day should the event fall on
     enum repeat_monthly_type {
+        NEVER,
         SAME_DAY, // eg every 25th
         SAME_WEEKDAY // eg every second tuesday
     }
@@ -25,6 +26,7 @@ public class DayEvent {
 
     // for repeated events, how long does it keep repeating
     enum repeated_until_type {
+        NEVER,
         FOREVER,
         UNTIL_DATE,
         N_REPETITIONS
@@ -38,4 +40,28 @@ public class DayEvent {
     // for events repeated a certain number of times, how many reps?
     private int repetition_step, repeated_reps;
 
+    public DayEvent() {}
+
+    public DayEvent(String n, Date begin, Date end) {
+        this.name = n;
+        this.begin_date = begin;
+        this.end_date = end;
+
+        this.repeated = repeat_type.NEVER;
+        this.repeated_monthly = repeat_monthly_type.NEVER;
+        this.repeated_until = repeated_until_type.NEVER;
+        this.repeated_until_date = end;
+        this.repeated_reps = 0;
+        this.repetition_step = 0;
+
+    }
+
+    public DayEvent(String n, Date begin, Date end, int repeat, int monthly, int until, Date until_date, int reps, int step) {
+        this.name = n;
+        this.begin_date = begin;
+        this.end_date = end;
+
+        this.repeated = repeat;
+
+    }
 }
