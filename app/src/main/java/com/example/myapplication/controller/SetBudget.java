a2460fb2304de7f401d8fdde9b8b6dcfe3d8f7f5
Tadashi24 <xfo904@usask.ca>
2025-03-03 09:02:47 -0600

    set budget initial implementation

--- app/src/main/java/com/example/myapplication/controller/SetBudget.java
+++ app/src/main/java/com/example/myapplication/controller/SetBudget.java
@@ -1,6 +1,10 @@
 package com.example.myapplication.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +13,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
+import com.example.myapplication.model.Food;
 
 public class SetBudget extends AppCompatActivity {
+
+    EditText shoppingBudget, foodBudget, entertainmentBudget, travelingBudget;
+    TextView addBudget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_set_budget);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        shoppingBudget = findViewById(R.id.shoppingBudget);
+        foodBudget = findViewById(R.id.foodBudget);
+        entertainmentBudget = findViewById(R.id.foodBudget);
+        travelingBudget = findViewById(R.id.travelingBudget);
+        addBudget = findViewById(R.id.addBudget);
+
+
+        addBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(SetBudget.this, Food.class));
+
+
+            }
         });
     }
 }
