a483dd9a178ae7ecc1caa5a6a48417430264f7c7
Sayed Farzaan Rafi Bhat <kfn036@usask.ca>
2025-03-02 18:11:26 -0600

    Merged database from Parker, and added required dependencies for functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,4 +52,11 @@ dependencies {
     annotationProcessor(libs.room.compiler)
     // optional - Test helpers
     testImplementation(libs.room.testing)
+
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.google.firebase.firestore)
 }
