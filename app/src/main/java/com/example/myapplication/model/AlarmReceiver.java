6b81aa6d3056bcfe850fae47c8ed7cbe91fafe76
Daniel Laszlo (job548) <job548@mail.usask.ca>
2025-03-25 21:10:33 -0600

    android doesn't allow activities to start in the background so I might need to scrap this idea

--- app/src/main/java/com/example/myapplication/model/AlarmReceiver.java
+++ app/src/main/java/com/example/myapplication/model/AlarmReceiver.java
@@ -1,15 +1,23 @@
 package com.example.myapplication.model;
 
+import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.AlarmClock;
+import android.provider.Settings;
 
 import com.example.myapplication.controller.AlarmActivity;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (!alarmManager.canScheduleExactAlarms()) {
+            Intent askIntent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+            context.startActivity(askIntent);
+        }
+
         Intent alarmIntent = new Intent(context, AlarmActivity.class);
         String jobName = intent.getStringExtra("jobName");
         String shiftName = intent.getStringExtra("shiftName");
